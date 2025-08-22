3e2adfa1544ef5a93a6d2fef2dfd76dbcaf054fd
Madhusha Prasad <madushaprasad21@gmail.com>
2025-08-22 17:26:31 +0530

    feat: add GSAP animations and update styles for smooth scrolling effect

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
 import styles from './styles.module.css'
 import Image from 'next/image'
+import gsap from 'gsap'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+import {useLayoutEffect, useRef} from 'react'
 
 function Index() {
+
+  const backgroundImage = useRef(null)
+  const introImage = useRef(null)
+
+  useLayoutEffect(()=>{
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeLine = gsap.timeline({
+      scrollTrigger:{
+        trigger:document.documentElement,
+        start:"top",
+        end:"+=500px",
+        scrub:true,
+        markers:true
+      }
+    })
+
+    timeLine
+    .from(backgroundImage.current,{clipPath:"inset(15%)"})
+    .to(introImage.current,{height:"200px"},0)
+
+  },[])
+
+
   return (
     <div className={styles.intro}>
-      <div className={styles.backgroundImage}>
+      <div ref={backgroundImage} className={styles.backgroundImage}>
         <Image src={'/images/background.jpeg'} fill={true} alt="background image" />
       </div>
       <div className={styles.introContainer}>
-        <div className={styles.introImage}>
+        <div ref={introImage} data-scroll data-scroll-speed="0.5" className={styles.introImage}>
           <Image src={'/images/intro.png'} fill={true} alt="background image" />
         </div>
-        <h1>Smooth Scroll</h1>
+        <h1 data-scroll data-scroll-speed="0.7">Smooth Scroll</h1>
       </div>
     </div>
   )
